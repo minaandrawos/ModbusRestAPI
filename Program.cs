6a3e58625ab06cb74791212cc4eb5ad11edf3892
Mina Andrawos <mina andrawos>
2016-08-29 20:45:47 -0700

    Initial check-in of module ModbusRestAPI

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace ModbusHandler
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			StartNancy();
+			Console.ReadLine();
+		}
+
+		static void StartNancy()
+		{
+			using (var host = new NancyHost(new Uri("http://localhost:1234")))
+			{
+				host.Start();
+				Console.WriteLine("Running on http://localhost:1234");
+				Console.ReadLine();
+			}
+		}
+	}
+}
---
+++ routes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Nancy.ModelBinding;
+using ModbusHandler;
+using Nancy;
+
+namespace ModbusHandler
+{
+	public class routes : Nancy.NancyModule
+	{
+		public routes()
+		{
+			Get["/"] = _ => "Hello to the Modbus REST API";
+
+			Post["/Read/{devicetype}"] = parameters => 
+			{
+				string t = parameters.devicetype;
+				var req = this.Bind<request>();
+				object result = new object();
+				try
+				{
+					switch (t.ToLower())
+					{
+						case "coils":
+							result = Coils.Instance.Read(req);
+							break;
+						case "holdingregisters":
+							result = HoldingRegisters.Instance.Read(req);
+							break;
+						case "inputregisters":
+							result = InputRegisters.Instance.Read(req);
+							break;
+						case "inputs":
+							result = Inputs.Instance.Read(req);
+							break;
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex);
+				}
+				return Response.AsJson(result);
+			};
+
+			Post["/Write/{devicetype}"] = parameters =>
+			{
+				string t = parameters.devicetype;
+				switch (t.ToLower())
+				{
+					case "coils":
+						var cwreq = this.Bind<writerequest<bool[]>>();
+						Coils.Instance.Write(cwreq);
+						break;
+					case "holdingregisters":
+						var rwreq = this.Bind<writerequest<ushort[]>>();
+						HoldingRegisters.Instance.Write(rwreq);
+						break;
+				}
+				return HttpStatusCode.OK;
+			};
+		}
+	}
+}
+
